84fb2dd29e9b4f2f8f71a80ef0941f349167727e
OlgaCheraneva <32261747+OlgaCheraneva@users.noreply.github.com>
2018-11-10 20:08:44 +0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -7,7 +7,12 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('Объект фильтра не является инстансом функции-конструктора Filter.');
+    }
+    this.invitedFriends = getFriends({ friends })
+        .filter(friend => filter.isEligible(friend));
+    this.index = 0;
 }
 
 /**
@@ -19,7 +24,9 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.apply(this, [friends, filter]);
+    this.invitedFriends = getFriends({ friends, maxLevel })
+        .filter(friend => filter.isEligible(friend));
 }
 
 /**
@@ -27,7 +34,9 @@ function LimitedIterator(friends, filter, maxLevel) {
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.isEligible = function () {
+        return true;
+    };
 }
 
 /**
@@ -36,7 +45,9 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.isEligible = function (friend) {
+        return friend.gender === 'male';
+    };
 }
 
 /**
@@ -45,8 +56,61 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.isEligible = function (friend) {
+        return friend.gender === 'female';
+    };
 }
+
+function getFriends({ friends, maxLevel = Infinity }) {
+    let result = [];
+    let currentLevelIndex = 1;
+    let currentLevelFriends = friends.filter(friend => friend.best)
+        .sort((a, b) => a.name.localeCompare(b.name));
+
+    while (currentLevelIndex <= maxLevel && currentLevelFriends.length > 0) {
+        result = result.concat(currentLevelFriends);
+        currentLevelFriends = getNextLevelFriends(currentLevelFriends, result, friends);
+        currentLevelIndex++;
+    }
+
+    return result;
+}
+
+function getNextLevelFriends(currentLevelFriends, invitedFriends, allFriends) {
+    const names = currentLevelFriends.reduce((accumulator, friend) => {
+        return accumulator.concat(friend.friends);
+    }, []);
+    const friends = names.map(name => {
+        return allFriends.find(friend => name === friend.name);
+    });
+    let currentIndex = 0;
+    let nextLevelFriends = friends.filter(friend => !invitedFriends.includes(friend));
+    nextLevelFriends.filter(friend => {
+        return nextLevelFriends.indexOf(friend) === currentIndex++;
+    });
+
+    return nextLevelFriends.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+const IteratorPrototype = {
+    invitedFriends: [],
+    index: 0,
+    done() {
+        return this.index >= this.invitedFriends.length;
+    },
+    next() {
+        if (this.done()) {
+            return null;
+        }
+
+        return this.invitedFriends[this.index++];
+    }
+};
+
+Object.setPrototypeOf(Iterator.prototype, IteratorPrototype);
+Object.setPrototypeOf(LimitedIterator.prototype, IteratorPrototype);
+Object.setPrototypeOf(MaleFilter.prototype, Filter.prototype);
+Object.setPrototypeOf(FemaleFilter.prototype, Filter.prototype);
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
